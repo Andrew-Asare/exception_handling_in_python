1feee5e25a468990dc68fcdeecdf596d84304d46
andrew <andrewasare98@gmail.com>
2021-03-29 15:25:01 +0100

    Exception handling in python

--- README.md
+++ README.md
@@ -3,3 +3,40 @@
 
 ### To understand the concept easily - 
 - Each block of code works as an if, elif, else block
+
+```
+# There is a built in method in python called open("")
+# file = open("orders.text") # looks for the file called orders.text
+# keyword try
+try:
+    file = open("orders.text")
+    print("file was found")
+except FileNotFoundError as errmsg:
+    print(f"the above block of code was not executed {errmsg}")
+    # raise
+# finally executes regardless of above conditions
+finally:
+    print("This is found, your task is to read the data and display it as a list")
+
+```
+
+- Please ensure to create an orders.text if does not exist
+
+| Mode |Description|
+| :----: |:---- |
+|'r' |This is the default mode. It Opens file for reading. |
+|'w' |This Mode Opens file for writing. If file does not exist, it creates a new file. If file exists it truncates the file.|
+|'x' |Creates a new file. If file already exists, the operation fails.|
+|'a' |Open file in append mode. If file does not exist, it creates a new file.|
+|'t' |This is the default mode. It opens in text mode.|
+|'b' |This opens in binary mode.
+|'+' |This will open a file for reading and writing (updating)|
+
+
+
+
+It is worth noting that the `+` operator can be used with
+
+
+### CRUD
+`create` `read` `update` `delete`
--- orders.py
+++ orders.py
@@ -1,1 +1,60 @@
-# Dealing with files and error/exception
+# # Dealing with files and error/exception handling
+#
+# # There is a built in method in python called open("")
+# # file = open("orders.text") # looks for the file called orders.text
+# # keyword try
+# try:
+#     file = open("orders.text")
+#     print("file was found")
+# except FileNotFoundError as errmsg:
+#     print(f"the above block of code was not executed {errmsg}")
+#     # raise
+# # finally executes regardless of above conditions
+# finally:
+#     print("This is found, your task is to read the data and display it as a list")
+#
+# second iteration:
+# Your task is to read the data and display it as a list
+# file = open("orders.text", "r")
+# content = file.read()
+# print(content)
+# third iteration:
+filename = "orders.text"
+# def read_and_print(filename):
+#     try:
+#         items = []
+#         file = open(filename, 'r')
+#         print('File found')
+#         for line in file:
+#             items.append(line.rstrip('\n'))
+#         print(items)
+#     except IOError:
+#         print('The above block was not executed')
+#     finally:
+#         print("Thank you")
+#
+
+#
+# def open_using_with_and_print(file):
+#     try:
+#         with open(file, "r") as file:
+#             for line in file.readlines():
+#                 print(line.rstrip('\n'))
+#     except FileNotFoundError:
+#         print("file cannot be found or directory is incorrect, please check the details provided")
+#         raise
+#     finally:
+#         print("\nPlease chose the items from the list  and enjoy your HAPPY MEAL")
+# open_using_with_and_print("orders.text")
+#
+# open_and_read_file(filename)
+
+
+#
+# def write_to_file(file, order_item):
+#     try:
+#         with open(file, "w") as file:
+#             file.write(order_item + '\n')
+#     except FileNotFoundError:
+#         print("file cannot be found or directory is incorrect, please check the details provided")
+#         raise
